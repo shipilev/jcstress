1ef70f75df1b7cad2d5128c64c384a95685fdf4b
Aleksey Shipilev <shipilev@amazon.de>
2024-11-29 20:06:36 +0100

    More Lazy work

--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/primitives/lazy/Lazy_04_BrokenOneShot.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/primitives/lazy/Lazy_04_BrokenOneShot.java
@@ -37,12 +37,24 @@ import java.util.function.Supplier;
 import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
 import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE_INTERESTING;
 
-/*
-    How to run this test:
-    $ java -jar jcstress-samples/target/jcstress.jar -t LazyTest
-*/
-
 public class Lazy_04_BrokenOneShot {
+
+    /*
+        How to run this test:
+            $ java -jar jcstress-samples/target/jcstress.jar -t Lazy_04
+    */
+
+    /*
+        ----------------------------------------------------------------------------------------------------------
+
+        Lazy_03_Basic gets is a whirlwind of three fields: factory, instance, set. Could we make it better by
+        signaling initialized state using `factory` field itself? For example, by `null`-ing it out when it
+        was used? This looks like a great idea, and it also conserves memory by not holding to the factory
+        once we are done with it.
+
+        We can write DCL based on `factory` field then. The release-acquire between releasing factory=null store
+        after first use and the acquire on fast path should give us the memory ordering we want.
+     */
 
     static class BrokenOneShotLazy<T> implements Lazy<T> {
         private volatile Supplier<T> factory;
@@ -68,14 +80,30 @@ public class Lazy_04_BrokenOneShot {
         }
     }
 
+    /*
+        ...and it does! The example works well on all platforms.
+
+        x86_64, AArch64:
+              RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+          data, data  745,085,384  100.00%  Acceptable  Seeing the proper data.
+     */
+
     @JCStressTest
     @State
-    @Outcome(id = {"data, data"}, expect = ACCEPTABLE, desc = "Seeing the proper data.")
+    @Outcome(id = "data, data", expect = ACCEPTABLE, desc = "Seeing the proper data.")
     public static class Basic {
         Lazy<Holder> lazy = new BrokenOneShotLazy<>(new HolderFactory());
         @Actor public void actor1(LL_Result r) { r.r1 = Lazy.map(lazy); }
         @Actor public void actor2(LL_Result r) { r.r2 = Lazy.map(lazy); }
     }
+
+    /*
+        ...and it handles nulls for us really well, since we do not depend on `instance` nullity.
+
+        x86_64, AArch64:
+                            RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+          null-holder, null-holder  842,580,424  100.00%  Acceptable  Seeing a null holder.
+     */
 
     @JCStressTest
     @State
@@ -86,16 +114,44 @@ public class Lazy_04_BrokenOneShot {
         @Actor public void actor2(LL_Result r) { r.r2 = Lazy.map(lazy); }
     }
 
+    /*
+        Except that this implementation is subtly broke under the racy publication of Lazy itself.
+        Changing `final` to `volatile` for `factory` field give us a *weaker* semantics in constructors:
+        the store to volatile field in constructor may not be visible under racy publication!
+        See AdvancedJMM_13_VolatileVsFinal for the basic example of this. So we see this:
+
+        AArch64:
+           RESULT        SAMPLES     FREQ       EXPECT  DESCRIPTION
+             data    654,125,625   29.68%   Acceptable  Trivial.
+      null-holder             32   <0.01%  Interesting  Seeing uninitialized holder!
+        null-lazy  1,550,108,527   70.32%   Acceptable  Lazy instance not seen yet.
+     */
+
     @JCStressTest
     @State
-    @Outcome(id = {"data"},        expect = ACCEPTABLE, desc = "Trivial.")
-    @Outcome(id = {"null-lazy"},   expect = ACCEPTABLE, desc = "Lazy instance not seen yet.")
-    @Outcome(id = {"null-holder"}, expect = ACCEPTABLE_INTERESTING, desc = "Seeing uninitialized holder!")
+    @Outcome(id = "data",        expect = ACCEPTABLE, desc = "Trivial.")
+    @Outcome(id = "null-lazy",   expect = ACCEPTABLE, desc = "Lazy instance not seen yet.")
+    @Outcome(id = "null-holder", expect = ACCEPTABLE_INTERESTING, desc = "Seeing uninitialized holder!")
     public static class RacyOneWay {
         Lazy<Holder> lazy;
         @Actor public void actor1()           { lazy = new BrokenOneShotLazy<>(new HolderFactory()); }
         @Actor public void actor2(L_Result r) { r.r1 = Lazy.map(lazy); }
     }
+
+    /*
+        ...and also the two-way race is also failing:
+
+        AArch64:
+                          RESULT        SAMPLES     FREQ       EXPECT  DESCRIPTION
+                      data, data    562,737,656   12.85%   Acceptable  Trivial.
+               data, null-holder             12   <0.01%  Interesting  Seeing uninitialized holder!
+                 data, null-lazy    695,112,068   15.88%   Acceptable  Lazy instance not seen yet.
+               null-holder, data             18   <0.01%  Interesting  Seeing uninitialized holder!
+          null-holder, null-lazy             24   <0.01%  Interesting  Seeing uninitialized holder!
+                 null-lazy, data    736,499,631   16.82%   Acceptable  Lazy instance not seen yet.
+          null-lazy, null-holder             34   <0.01%  Interesting  Seeing uninitialized holder!
+            null-lazy, null-lazy  2,383,387,321   54.44%   Acceptable  Lazy instance not seen yet.
+     */
 
     @JCStressTest
     @State
--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/primitives/lazy/Lazy_05_AtomicRefOneShot.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/primitives/lazy/Lazy_05_AtomicRefOneShot.java
@@ -40,12 +40,20 @@ import java.util.function.Supplier;
 
 import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
 
-/*
-    How to run this test:
-    $ java -jar jcstress-samples/target/jcstress.jar -t LazyTest
-*/
-
 public class Lazy_05_AtomicRefOneShot {
+
+   /*
+        How to run this test:
+            $ java -jar jcstress-samples/target/jcstress.jar -t Lazy_04
+    */
+
+    /*
+        ----------------------------------------------------------------------------------------------------------
+
+        Lazy_04_BrokenOneShot shows us we cannot have `volatile` field have the `final` semantics we need
+        to protect from constructor races. If only we had a way to get both at the same time. And we have one:
+        we can have a volatile wrapper that we put into final field.
+     */
 
     static class AtomicRefLazy<T> implements Lazy<T> {
         private final AtomicReference<Supplier<T>> factoryRef;
@@ -71,6 +79,12 @@ public class Lazy_05_AtomicRefOneShot {
         }
     }
 
+    /*
+      RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+  data, data  714,969,544  100.00%  Acceptable  Trivial.
+
+     */
+
     @JCStressTest
     @State
     @Outcome(id = "data, data", expect = ACCEPTABLE, desc = "Trivial.")
@@ -79,6 +93,12 @@ public class Lazy_05_AtomicRefOneShot {
         @Actor public void actor1(LL_Result r) { r.r1 = Lazy.map(lazy); }
         @Actor public void actor2(LL_Result r) { r.r2 = Lazy.map(lazy); }
     }
+
+/*
+                    RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+  null-holder, null-holder  838,586,824  100.00%  Acceptable  Seeing a null holder.
+
+ */
 
     @JCStressTest
     @State
@@ -89,6 +109,12 @@ public class Lazy_05_AtomicRefOneShot {
         @Actor public void actor2(LL_Result r) { r.r2 = Lazy.map(lazy); }
     }
 
+    /*
+     RESULT        SAMPLES     FREQ      EXPECT  DESCRIPTION
+       data    632,710,141   32.62%  Acceptable  Trivial.
+  null-lazy  1,307,055,563   67.38%  Acceptable  Lazy instance not seen yet.
+     */
+
     @JCStressTest
     @State
     @Outcome(id = "data",      expect = ACCEPTABLE, desc = "Trivial.")
@@ -98,6 +124,15 @@ public class Lazy_05_AtomicRefOneShot {
         @Actor public void actor1()           { lazy = new AtomicRefLazy<>(new HolderFactory()); }
         @Actor public void actor2(L_Result r) { r.r1 = Lazy.map(lazy); }
     }
+
+    /*
+                RESULT        SAMPLES     FREQ      EXPECT  DESCRIPTION
+            data, data    482,246,211   12.03%  Acceptable  Trivial.
+       data, null-lazy    644,270,368   16.07%  Acceptable  Lazy instance not seen yet.
+       null-lazy, data    693,570,498   17.30%  Acceptable  Lazy instance not seen yet.
+  null-lazy, null-lazy  2,189,716,247   54.61%  Acceptable  Lazy instance not seen yet.
+
+     */
 
     @JCStressTest
     @State
